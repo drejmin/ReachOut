0270204dadbfffc07e760d2f82c099f339e47978
drejmin <djjamn@gmail.com>
2023-09-13 09:50:59 -0700

    modified:   reachout/server.js

--- reachout/server.js
+++ reachout/server.js
@@ -16,112 +16,40 @@ const io = socketio(server);
 
 const port = process.env.PORT || 3001;
 
-// Initialize Middleware
-initMiddleware();
 
-// Initialize MongoDB
-initMongoDB();
+app.use(cors());
+app.use(express.json());
 
-// Initialize API Routes
-initApiRoutes();
 
-// Initialize Socket.IO
-initSocketIO();
 
-// Initialize the server
-startServer();
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB connection established successfully');
+});
 
-function initMiddleware() {
-  app.use(cors());
-  app.use(express.json());
-}
+const validPassword = await bcrypt.compare(req.body.password, user.password);
+if (!validPassword) return res.status(400).send('Username or Password is Wrong');
 
-function initMongoDB() {
-  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-  const connection = mongoose.connection;
-  connection.once('open', () => {
-    console.log('MongoDB connection established successfully');
-  });
-}
+const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+res.header('Authorization', token).send(token);
 
-function initApiRoutes() {
-  // User Schema
-  const userSchema = new mongoose.Schema({
-    username: String,
-    password: String
-  });
-  const User = mongoose.model('User', userSchema);
+app.use('/message', msgRouter);
 
-  app.use('/message', msgRouter);
+io.on('connection', (socket) => {
+  console.log(`Socket ${socket.id} connected`);
 
-  // Register user
-  app.post('/register', async (req, res) => {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-    const user = new User({
-      username: req.body.username,
-      password: hashedPassword
-    });
-
-    try {
-      await user.save();
-      res.send('User Registered');
-    } catch {
-      res.status(400).send('Registration Failed');
-    }
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message);
   });
 
-  // Login
-  app.post('/login', async (req, res) => {
-    const user = await User.findOne({ username: req.body.username });
-    if (!user) return res.status(400).send('Username or Password is Wrong');
-
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send('Username or Password is Wrong');
-
-    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-    res.header('Authorization', token).send(token);
+  socket.on('disconnect', () => {
+    console.log(`Socket ${socket.id} disconnected`);
   });
+});
 
-  // Protected Route
-  app.get('/protected', authenticateJWT, (req, res) => {
-    res.send('Protected Route Accessed');
-  });
-
-  // Authentication middleware
-  function authenticateJWT(req, res, next) {
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).send('Access Denied');
-
-    try {
-      const verified = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = verified;
-      next();
-    } catch {
-      res.status(400).send('Invalid Token');
-    }
-  }
-}
-
-function initSocketIO() {
-  io.on('connection', (socket) => {
-    console.log(`Socket ${socket.id} connected`);
-  
-    socket.on('sendMessage', (message) => {
-      io.emit('message', message);
-    });
-  
-    socket.on('disconnect', () => {
-      console.log(`Socket ${socket.id} disconnected`);
-    });
-  });
-}
-
-function startServer() {
-  server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-}
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 
