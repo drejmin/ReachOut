28e47569ab1df590f01ba2b9b28636d6414cb64e
drejmin <djjamn@gmail.com>
2023-10-23 15:37:39 -0700

    modified:   reachout/controllers/chatroom.js 	modified:   reachout/controllers/message.js 	deleted:    reachout/models/chatroom.js 	modified:   reachout/src/pages/App/App.jsx

--- reachout/controllers/message.js
+++ reachout/controllers/message.js
@@ -1,35 +1,89 @@
 const Message = require ("../models/message.js");
+const User = require("../models/user.js");
+const Chat = require("../models/chat.js");
+const expressAsyncHandler = require("express-async-handler");
 
-module.exports ={
-    createMsg,
-    getMsg,
-}
-async function createMsg(req, res){
-    
-    const newMsg = new Message(req.body);
 
+const allMsgs = expressAsyncHandler( async(req,res)=>{
     try{
-        await newMsg.save();
-        res.status(200).json(newMsg);
+        const messages = await Message.find({ chat: req.params.chatId})
+            .populate("sender", "name avatar email")
+            .populate("chat");
+        res.json(messages);
     } catch (error){
-        res.status(400).json({
-            message: error.message,
-        });
+        res.status(400);
+        throw new Error(error.message)
     }
-}
+})
 
-async function getMsg(req,res){
-    try{
-        const messages = await ChatMsg.find({
-            RoomId: req.params.RoomId,
-        });
-        res.status(200).json(messages);
-    }catch (error){
-        res.status(400).json({
-            message: error.message,
-        });
+const sendMsg = expressAsyncHandler(async (req, res)=>{
+    const {content, chatId} = req.body;
+
+    if (!content || !chatId){
+        console.log("Invalid Data Requested");
+        return res.sendStatus(400);
     }
+
+    var newMsg = {
+        sender: req.user._id,
+        content: content,
+        chat: chatId,
+    };
+
+    try {
+        var message = await Message.create(newMsg);
+
+        message = await message.populate("sender", "name avatar"). execPopulate();
+        message = await message.populate("chat"). execPopulate();
+        message = await User.populate(message, {
+            path: "chat.users",
+            select: "name avatar email",
+        });
+
+        await Chat.findByIdAndUpdate(req.body.chatId, {latestMessage: message});
+
+        res.json(message);
+    } catch(error){
+        res.status(400);
+        throw new Error(error.message);
+    }
+    });
+
+    module.exports = { 
+        allMsgs,
+        sendMsg,
+    };
     
-}
+// module.exports ={
+//     createMsg,
+//     getMsg,
+// }
+// async function createMsg(req, res){
+    
+//     const newMsg = new Message(req.body);
+
+//     try{
+//         await newMsg.save();
+//         res.status(200).json(newMsg);
+//     } catch (error){
+//         res.status(400).json({
+//             message: error.message,
+//         });
+//     }
+// }
+
+// async function getMsg(req,res){
+//     try{
+//         const messages = await ChatMsg.find({
+//             RoomId: req.params.RoomId,
+//         });
+//         res.status(200).json(messages);
+//     }catch (error){
+//         res.status(400).json({
+//             message: error.message,
+//         });
+//     }
+    
+// }
 
 
--- reachout/src/pages/App/App.jsx
+++ reachout/src/pages/App/App.jsx
@@ -1,34 +1,38 @@
 //App.js
 import "./App.css";
 import {Routes, Route} from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-
-import NavBar from "../../components/NavBar/NavBar";
+import {React, useState, useEffect } from 'react';
 import HomePage from '../HomePage/HomePage';
-import RoomPage from '../RoomPage/RoomPage';
 import ChatPage from '../ChatPage/ChatPage';
+// import NavBar from "../../components/NavBar/NavBar";
+// import RoomPage from '../RoomPage/RoomPage';
+
 
 export default function App() {
 
 
   return (
     <main className="App">
-       user ?
-        <>
-        <NavBar user={user} />
-        <Routes>
-            {/* Route components in here */}
-            <Route path="/" element={<HomePage 
-            user={user} 
-            setUser={setUser} 
-            room={room}
-            setRoom={setRoom}
-            socket={socket} />} />
-            <Route path="/chat/room" element={<RoomPage />} />
-            <Route path='/chat' element={<ChatPage user={user} room={room} socket={socket} />}/>
-        </Routes>
-        </>
-        </main>
+      <Route path="/" Component={HomePage} exact/>
+      <Route path="/chats" Component={ChatPage} />
+    </main>
+    // <main className="App">
+    //    user ?
+    //     <>
+    //     <NavBar user={user} />
+    //     <Routes>
+    //         {/* Route components in here */}
+    //         <Route path="/" element={<HomePage 
+    //         user={user} 
+    //         setUser={setUser} 
+    //         room={room}
+    //         setRoom={setRoom}
+    //         socket={socket} />} />
+    //         <Route path="/chat/room" element={<RoomPage />} />
+    //         <Route path='/chat' element={<ChatPage user={user} room={room} socket={socket} />}/>
+    //     </Routes>
+    //     </>
+    //     </main>
         );
       }
     
